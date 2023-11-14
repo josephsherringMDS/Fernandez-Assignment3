afe106765da0066bc20762681595d2653b4ffbe6
matthewgalenz <matthewgalenz@gmail.com>
2023-11-14 20:16:37 +1300

    UnityVersion1
    First one

--- Assets/Hospital.cs
+++ Assets/Hospital.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 
-namespace Assets
+public class Hospital : MonoBehaviour
 {
-    internal class Hospital
+    // Start is called before the first frame update
+    void Start()
     {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
--- Assets/HospitalDecorator.cs
+++ Assets/HospitalDecorator.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
 
-namespace Assets
+public class HospitalDecorator : MonoBehaviour
 {
-    internal class HospitalDecorator
+    // Start is called before the first frame update
+    void Start()
     {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
